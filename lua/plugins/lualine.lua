6159a8a4c40f83e39fd9c97c67bb63c7acc2d409
Tony15246 <a18061453582@gmail.com>
2024-08-09 00:00:21 +0800

    solve tab format problem

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,25 +1,25 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "nord",
-			},
-			sections = {
-				lualine_x = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { fg = "#ff9e64" },
-					},
-					{
-						"%S", -- 显示部分命令的组件
-					},
-				},
-			},
-		})
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "nord",
+            },
+            sections = {
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                    {
+                        "%S", -- 显示部分命令的组件
+                    },
+                },
+            },
+        })
 
-		vim.o.showcmdloc = "statusline"
-	end,
+        vim.o.showcmdloc = "statusline"
+    end,
 }
